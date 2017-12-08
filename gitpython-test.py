013a9d6f9b7df03b8272f5bbdaef5e9366a6c67b
Binlong Li <bil@microsoft.com>
2017-12-07 16:39:56 -0800

    add commited datetime.

--- gitpython-test.py
+++ gitpython-test.py
@@ -3,3 +3,4 @@ repo = Repo("C:/Experiment/powershell/pstoolbox")
 assert not repo.bare
 commit_hist = list(repo.iter_commits('master'))
 commits = [commit for commit in commit_hist if 'binlong' in commit.author.name.lower()]
+commits[0].committed_datetime
