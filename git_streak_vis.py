6c7db828affc78ad94106e4e2f708fb6a40a396c
Binlong <bil@microsoft.com>
2017-12-15 17:28:07 -0800

    add python template to generate the html file.

--- git_streak_vis.py
+++ git_streak_vis.py
@@ -2,6 +2,8 @@
 This module will visualize the output from git_streak_ino, such as overall commits,
 longest/current streak etc.
 """
+import os
+from jinja2 import Environment, FileSystemLoader
 from git_streak_info import agg_repo_commits, last_year_table, get_commit_stats
 
 
@@ -15,8 +17,43 @@ def print_stats(stats):
     print("Current Streak {0} Days, from {1} to {2}".format(stats[2][0], \
                                                                 stats[2][1], stats[2][2]))
 
-if __name__ == "__main__":
+def main():
+    STATS = gather_date()
+    print_stats(STATS)
+
+def gather_data():
     RES_TABLE = agg_repo_commits()
     FULL_COMMIT_TABLE = last_year_table(RES_TABLE)
     STATS = get_commit_stats(FULL_COMMIT_TABLE)
-    print_stats(STATS)
+    return STATS
+
+
+def generate_html():
+    THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+    template_path = os.path.join(THIS_DIR, 'templates')
+    env = Environment(loader=FileSystemLoader(template_path), 
+                        trim_blocks=True)
+    template = env.get_template('test_template.html')
+
+    info = {
+        'title': 'hello world 2',
+        'months': ['DEC', 'JAN', 'FEB'],
+        'offsets': ['', ''],
+        'days': [
+            {
+                "date": '2016-12-14',
+                "commits": 5,
+                "activity": 3
+            },
+            {
+                'date': '2016-12-15',
+                'commits': 4,
+                "activity": 2
+            }
+        ]
+
+    }
+    print(template.render(info))
+
+if __name__ == "__main__":
+    generate_html()
